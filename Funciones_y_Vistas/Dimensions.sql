f1484681e75bfa5e71bb14afbfcb5ed779346194
OliverJPR <33287824+OliverJPR@users.noreply.github.com>
2019-03-12 20:39:01 -0400

    Añadiendo dimension fecha

--- Funciones_y_Vistas/Dimensions.sql
+++ Funciones_y_Vistas/Dimensions.sql
@@ -5,3 +5,26 @@ venue_name as Nombre_Estadio
 	FROM teams;
 from teams
 );
+
+CREATE VIEW Dimension_Tiempo AS (
+SELECT 
+to_char("game_date",'DD-MM-YYYY') as "Fecha Juego",
+to_char("game_date",'DD') as "Dia Juego",
+to_char("game_date",'DAY') as "Dia Semana Juego",
+to_char("game_date",'MONTH') as "Mes Juego",
+to_char("game_date",'YYYY') as "Año Juego",
+"start_time" as " Hora Inicio",
+to_char("start_time",'HH24') as "Hora",
+"elapsed_time_min" as "Duracion (MIN)",
+to_char(("elapsed_time_min" / 60.0),'9.9') as "Duracion (HORAS)",
+case 
+    when to_number(to_char("game_date",'MM'),'99' ) in (12,1,2) then 'WINTER'
+    when to_number(to_char("game_date",'MM'),'99' ) in (3,4,5) then 'SPRING'
+    when to_number(to_char("game_date",'MM'),'99' )  in (6,7,8) then 'SUMMER'
+    when to_number(to_char("game_date",'MM'),'99' )  in (9,10,11) then 'FALL'
+end as "Temporada Climatica"
+
+FROM "games"
+
+
+);
