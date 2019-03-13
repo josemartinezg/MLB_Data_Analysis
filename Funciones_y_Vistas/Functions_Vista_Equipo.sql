4dc8eae6d3be3a0adbbd3642956b24398b986235
OliverJPR <33287824+OliverJPR@users.noreply.github.com>
2019-03-13 16:32:58 -0400

    Cantidad Evento pitcher

--- Funciones_y_Vistas/Functions_Vista_Equipo.sql
+++ Funciones_y_Vistas/Functions_Vista_Equipo.sql
@@ -384,4 +384,21 @@ where p.result_of_pitch = resultPitch and pl = ab.pitcher_id
 group by pn.player_id;
 end
 $$ LANGUAGE plpgsql;
+/*======================= Cantidad de eventos por pitcher (works)=============================*/
+create or replace function cantidadEvento(pl double precision ,evento text)
+returns TABLE  (
+  result bigint
+)
+as $$
+begin
+  return query SELECT count(ab.event) as result
+FROM pitches p
+INNER JOIN atbats ab on ab.ab_id = p.ab_id
+INNER JOIN player_name pn on pn.player_id = ab.pitcher_id
+where ab.event = evento and pl = ab.pitcher_id
+group by pn.player_id;
+end
+$$ LANGUAGE plpgsql;
+
+
 
