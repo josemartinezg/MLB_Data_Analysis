2be2144f4d28526b16141ad1ce0c122555042f27
OliverJPR <33287824+OliverJPR@users.noreply.github.com>
2019-03-12 23:38:37 -0400

    Dimension Lanzador (no terminada)

--- Funciones_y_Vistas/Dimensions.sql
+++ Funciones_y_Vistas/Dimensions.sql
@@ -28,3 +28,29 @@ FROM "games"
 
 
 );
+
+
+CREATE VIEW Dimension_Lanzador AS (
+SELECT  
+ab.pitcher_id as "Id Lanzador",
+concat(pn.first_name,' ', pn.last_name) as "Nombre Lanzador",
+case
+    when ab.p_throws = 'L' then 'Izquierda'
+    else 'Derecha'
+end as "Mano Lanzador",
+--bolas
+--strikes
+--AVG oposicion*
+to_char(avg(p.break_angle),'9999.99')as "Break Angle Prom.",
+to_char(avg(p.break_lengh),'9999.99')as "Break Length Prom.",
+sum(p.current_team_score) as "Carreras Permitidas",
+to_char(avg(p.end_Speed),'9999.99')as "Velocidad Promedio",
+to_char(avg(p.spin_Rate),'9999.99')as "Spin Rate Promedio",
+to_char(avg(p.nasty),'9999.99')as "Nasty Promedio",
+--hr permitidos
+--Promedio Bateo
+FROM pitches p
+INNER JOIN atbats ab on ab.ab_id = p.ab_id
+INNER JOIN player_name pn on pn.player_id = ab.pitcher_id
+GROUP BY ab.pitcher_id,pn.first_name,pn.last_name,ab.p_throws
+);
